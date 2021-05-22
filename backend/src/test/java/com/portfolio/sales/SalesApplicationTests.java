5cb705d0cbaad5a4c9856aaeb2a7c33b328d8cd7
FelipeGadelha <felipegadelha90@gmail.com>
2021-05-22 15:57:36 -0300

    created project

---
+++ backend/src/test/java/com/portfolio/sales/SalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
