e7eec716458f23ffa537df71642d088e3c84912d
FelipeGadelha <felipegadelha90@gmail.com>
2021-05-23 20:29:42 -0300

    add sonar properties

--- backend/src/main/java/com/portfolio/sales/domain/repository/SaleRepository.java
+++ backend/src/main/java/com/portfolio/sales/domain/repository/SaleRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.portfolio.sales.domain.entity.Sale;
-import com.portfolio.sales.domain.entity.Seller;
 
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long>{
