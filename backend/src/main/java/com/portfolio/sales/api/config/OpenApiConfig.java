8d4abd06d2732e52b41442d8f1d82b94ada0aee9
FelipeGadelha <felipegadelha90@gmail.com>
2021-05-22 23:14:41 -0300

    created application.yml and entity class

---
+++ backend/src/main/java/com/portfolio/sales/api/config/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package com.portfolio.sales.api.config;
+
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+public class OpenApiConfig {
+	
+	@Bean
+	public OpenAPI springSchoolOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("Sales API")
+						.description("Spring DSDelivery API sample application")
+						.version("v1.0")
+						.contact(new Contact()
+								.name("Felipe Gadelha Diniz Da Silva")
+								.url("https://www.linkedin.com/in/felipe-gadelha-diniz-da-silva-aaaa4a158/")
+								.email("felipegadelha90@gmail.com"))
+						.license(new License()
+								.name("Apache 2.0")
+								.url("https://www.apache.org/licenses/LICENSE-2.0")))
+				.externalDocs(new ExternalDocumentation()
+						.description("Spring Sales Github Documentation")
+						.url("https://github.com/FelipeGadelha/sales"));
+
+	}
+
+}
--- backend/src/main/java/com/portfolio/sales/domain/model/Sale.java
+++ backend/src/main/java/com/portfolio/sales/domain/model/Sale.java
@@ -1,10 +1,94 @@
 package com.portfolio.sales.domain.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
+@Table(name = "tb_sales")
 public class Sale {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
+	private Integer visited;
+	private Integer deals;
+	private BigDecimal amount;
 	
+	@Temporal(TemporalType.DATE)
+	private LocalDate date;
+	
+	@ManyToOne
+	@JoinColumn(name = "seller_id")
+	private Seller seller;
+
+	@Deprecated
+	public Sale() {	}
+	
+	public Sale(Long id, Integer visited, Integer deals, BigDecimal amount, LocalDate date, Seller seller) {
+		this.id = id;
+		this.visited = visited;
+		this.deals = deals;
+		this.amount = amount;
+		this.date = date;
+		this.seller = seller;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+
+	public Integer getDeals() {
+		return deals;
+	}
+
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public Seller getSeller() {
+		return seller;
+	}
+
+	public void setSeller(Seller seller) {
+		this.seller = seller;
+	}
 }
