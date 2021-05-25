3261e72657b5425d767e4d443b5e012903a21401
FelipeGadelha <felipegadelha90@gmail.com>
2021-05-25 17:29:23 -0300

    correcting baseUrl

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080/api/sales';
+const BASE_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080/api/sales';
 
 const api = axios.create({
   baseURL: BASE_URL,
