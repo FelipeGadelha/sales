95463047b49100428f55e655203e52cfe1393158
FelipeGadelha <felipegadelha90@gmail.com>
2021-05-25 17:18:05 -0300

    correcting env var

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080/api/sales';
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080/api/sales',
+  baseURL: BASE_URL,
 })
 
 export default api;
